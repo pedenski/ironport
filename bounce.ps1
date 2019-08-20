831c44fd920f51c57ba0018e070c71872654df2c
pedenski <pmzildjian@yahoo.com>
2019-08-20 17:42:10 +0800

    Add files via upload

--- bounce.ps1
+++ bounce.ps1
@@ -1,12 +1,24 @@
+
+#BOUNCE TO CSV
+write-host "====================="
+write-host "START: S BOUNCE TO CSV"
+write-host "====================="
 Cat *.s | sc ball.s #Consolidate all .S
 (Get-Content 'ball.s') | Where-Object { $_ -match 'careserror' } | Set-Content 'ball.s' #Check per line and get only lines with carerserror
 Set-Content -Path "ball.s" -Value (get-content -Path "ball.s" | Select-String -Pattern 'delayed' -NotMatch) #Remove lines with delayed 
 (Get-content 'ball.s') | ForEach-Object { $_ -replace ',',':::'} | Set-Content 'ball.s' #replace all comma (,) with (:::) this avoids CSV to delimit comma and separate it per column.
-import-csv ball.s | export-csv balls.csv  -NoTypeInformation #conver *.s by exporting to csv
-write-host "Conversion Done"
+import-csv ball.s | export-csv balls.csv  -NoTypeInformation #convert *.s by exporting to csv
+write-host "====================="
+write-host "DONE: S BOUNCE TO CSV"
+write-host "====================="
 
 
-#Excel
+
+
+#GETTING MID AND STATE FROM BOUNCE
+write-host "====================================="
+write-host "START: GET MID AND STATE FROM BOUNCE"
+write-host "====================================="
 $thispath = Get-Location
 write-host "opening $thispath\balls.csv"
 $excel = new-object -comobject Excel.Application
@@ -22,12 +34,14 @@ $worksheet.range("B1:B$colCount").formula = '=MID(A1,FIND("MID",A1)+4,9)' #add f
 $worksheet.range("C1:C$colCount").formula = 
 '=IF(ISNUMBER(SEARCH("spam content blocked",A1)),"rejected by destination", IF(OR(ISNUMBER(SEARCH({"does not exist","is not exist","doesnt have","aborted","expired","no such user","mailbox unavailable"},A1))),"invalid email",IF(OR(ISNUMBER(SEARCH({"dns soft","dns hard"},A1))),"invalid domain",IF(ISNUMBER(SEARCH("disabled",A1)),"mailbox disabled",IF(ISNUMBER(SEARCH("quota",A1)),"mailbox full","rejected by destination")))))'
 
-
-
 $workbook.SaveAs("$thispath\bounce.csv")
 
 $workbook.Close($false)
 $excel.Quit()
+
+write-host "======================================="
+write-host "DONE: GETTING MID AND STATE FROM BOUNCE"
+write-host "========================================"
 
 
 # need to release COM references and ensures no remaining references exist which will keep Excel.exe open 
@@ -40,8 +54,35 @@ $excel.Quit()
 
 Remove-Variable -Name excel
 
+
+#ADD COLUMN DATE MID STATE
+
+write-host "====================================="
+write-host "START: ADD COLUMN DATE / MID / STATE"
+write-host "====================================="
+
+$filedata = import-csv 'bounce.csv' -Header "date","MID","State"
+$filedata | export-csv 'bounce.csv' -NoTypeInformation
+
+write-host "====================================="
+write-host "DONE: ADD COLUMN DATE / MID / STATE"
+write-host "====================================="
+
+#DELETE UNECESSARY FILE
+
 Remove-Item "balls.csv"
 Remove-Item "ball.s"
 
 
 
+#CORRELATION OF MESSAGE TRACKING AND BOUNCE
+write-host "====================================="
+write-host "START: CORRELATION OF STATE (VLOOKUP)"
+write-host "====================================="
+
+$data = @{}
+Import-Csv 'bounce.csv' | ForEach-Object { $data[$_.MID] = $_.State }
+Import-Csv 'all.csv' |  Select-Object *, @{n='State';e={if ($data.Contains($_.MID)) {$data[$_.MID]} else {'success'}}} | export-csv 'gg.csv' -NoTypeInformation
+write-host "====================================="
+write-host "DONE: CORRELATION OF STATE (VLOOKUP)"
+write-host "======================================"
